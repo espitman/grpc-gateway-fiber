51fbd23e0cc38701e27d02e0530bb2a404607dd4
saeed heidari <s.heidari@jabama.ir>
2024-03-23 20:06:59 +0330

    feat: check enalble

--- handler_price_service.go
+++ handler_price_service.go
@@ -19,35 +19,12 @@ func newPriceServiceHandler(pb pricepb.PriceServiceClient) *priceServiceHandler 
 }
 
 func (h *priceServiceHandler) V1CalendarGet(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1CalendarGetRequest
 	_ = c.QueryParser(&reqDto)
 
 	res, err := h.pb.V1CalendarGet(ctx, &reqDto)
-
-	if err != nil {
-		st, _ := status.FromError(err)
-		log.Println("err:", st.Code(), st.Message(), st.Details())
-		return c.Status(fiber.StatusBadRequest).Send(nil)
-	}
-	return c.JSON(res)
-}
-
-func (h *priceServiceHandler) V1DayCreate(c *fiber.Ctx) error {
-
-	header := metadata.New(map[string]string{"authorization": "saeed"})
-	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
-	var reqDto pricepb.V1DayCreateRequest
-	_ = c.QueryParser(&reqDto)
-	_ = c.ParamsParser(&reqDto)
-	_ = c.BodyParser(&reqDto)
-
-	res, err := h.pb.V1DayCreate(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -57,17 +34,13 @@ func (h *priceServiceHandler) V1DayCreate(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1DayDisable(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1DayEnableDisableRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1DayDisable(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -77,17 +50,13 @@ func (h *priceServiceHandler) V1DayDisable(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1DayEnable(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1DayEnableDisableRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1DayEnable(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -97,17 +66,13 @@ func (h *priceServiceHandler) V1DayEnable(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1DayGuarantee(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1DayGuaranteeRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1DayGuarantee(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -117,17 +82,13 @@ func (h *priceServiceHandler) V1DayGuarantee(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1DayUnGuarantee(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1DayUnGuaranteeRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1DayUnGuarantee(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -137,17 +98,13 @@ func (h *priceServiceHandler) V1DayUnGuarantee(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1PriceCreate(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1PriceCreateRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1PriceCreate(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -157,16 +114,13 @@ func (h *priceServiceHandler) V1PriceCreate(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1PriceGet(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1PriceGetRequest
 
 	_ = c.ParamsParser(&reqDto)
 
 	res, err := h.pb.V1PriceGet(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
@@ -176,17 +130,13 @@ func (h *priceServiceHandler) V1PriceGet(c *fiber.Ctx) error {
 }
 
 func (h *priceServiceHandler) V1PriceUpdate(c *fiber.Ctx) error {
-
 	header := metadata.New(map[string]string{"authorization": "saeed"})
 	ctx := metadata.NewOutgoingContext(c.Context(), header)
-
 	var reqDto pricepb.V1PriceCreateRequest
 	_ = c.QueryParser(&reqDto)
 	_ = c.ParamsParser(&reqDto)
 	_ = c.BodyParser(&reqDto)
-
 	res, err := h.pb.V1PriceUpdate(ctx, &reqDto)
-
 	if err != nil {
 		st, _ := status.FromError(err)
 		log.Println("err:", st.Code(), st.Message(), st.Details())
