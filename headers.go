32c31b34f8c76d34f432ca4248cd995babbebb83
saeed heidari <s.heidari@jabama.ir>
2024-03-24 13:41:05 +0330

    feat: use uuid on header

--- headers.go
+++ headers.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"google.golang.org/grpc/metadata"
 )
 
 func getCtx(fctx fiberCtx) context.Context {
 	reqHeaders := fctx.GetReqHeaders()
-	header := metadata.New(map[string]string{"authorization": reqHeaders["Authorization"][0]})
+
+	headers := make(map[string]string)
+	headers["authorization"] = reqHeaders["Authorization"][0]
+	headers["trace-id"] = uuid.NewString()
+
+	header := metadata.New(headers)
 	ctx := metadata.NewOutgoingContext(fctx.Context(), header)
 	return ctx
 }
